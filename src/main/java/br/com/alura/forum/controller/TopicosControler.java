98b30807961622706e4a70b69ae6f18d39c58a8d
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-08-16 16:49:52 -0300

    primeira injecao de dependencia e criacao de repository

--- src/main/java/br/com/alura/forum/controller/TopicosControler.java
+++ src/main/java/br/com/alura/forum/controller/TopicosControler.java
@@ -1,23 +1,24 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
-@Controller
+@RestController
 public class TopicosControler {
+	
+	@Autowired
+	private TopicoRepository repository;
 
 	@RequestMapping("topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida Permanente", "Preciso de ajuda em SOLID", new Curso("Java Solid", "Backend"));
-		
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDTO> lista() {
+		List<Topico> topicos = repository.findAll();
+		return TopicoDTO.converter(topicos);
 	}
 }
