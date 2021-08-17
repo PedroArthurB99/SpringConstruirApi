93382072b5446a26645133efc00c50a6a1e1b634
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-08-17 16:40:55 -0300

    spring security - autenticacao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$UhelFnyYxKCmpbbtb1KKluMDfrrwackH7JkYqgsm49h4SpsPeDT9W');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
