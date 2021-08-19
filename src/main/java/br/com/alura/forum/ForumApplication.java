e1bb4e7684353f82642cd25128214e3ca48b9b2a
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-08-18 23:47:33 -0300

    deploy com variáveis de ambiente

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCaching
 @EnableSwagger2
 public class ForumApplication {
+//para o build do projeto gerar um arquivo .war devemos extender de SpringBootServerInitializer e sobrescrever o método configure
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
